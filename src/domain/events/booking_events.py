e1ffb8de0ffd2093b0018cacd9239a3d40f26c53
Tanq-1 <tanquiry@gmail.com>
2026-05-19 20:22:51 +0700

    fix: corrected arguments in booking_events.py

--- src/domain/events/booking_events.py
+++ src/domain/events/booking_events.py
@@ -1,15 +1,18 @@
 from datetime import datetime, timezone
 from uuid import UUID, uuid4
 
+from src.domain.value_objects.money import Money
+
 
 class TicketReserved:
     def __init__(
         self,
         booking_id: UUID,
         event_id: UUID,
-        customer_id: str,
+        customer_id: UUID,
         ticket_category_id: UUID,
         quantity: int,
+        total_price: Money,
     ):
         self.id = uuid4()
         self.occurred_at = datetime.now(timezone.utc)
@@ -19,19 +22,36 @@ class TicketReserved:
         self.customer_id = customer_id
         self.ticket_category_id = ticket_category_id
         self.quantity = quantity
+        self.total_price = total_price
 
 
 class BookingPaid:
-    def __init__(self, booking_id: UUID):
+    def __init__(
+        self,
+        booking_id: UUID,
+        customer_id: UUID,
+        event_id: UUID,
+        total_price: Money,
+    ):
         self.id = uuid4()
         self.occurred_at = datetime.now(timezone.utc)
 
         self.booking_id = booking_id
+        self.customer_id = customer_id
+        self.event_id = event_id
+        self.total_price = total_price
 
 
 class BookingExpired:
-    def __init__(self, booking_id: UUID):
+    def __init__(
+        self,
+        booking_id: UUID,
+        customer_id: UUID,
+        event_id: UUID,
+    ):
         self.id = uuid4()
         self.occurred_at = datetime.now(timezone.utc)
 
         self.booking_id = booking_id
+        self.customer_id = customer_id
+        self.event_id = event_id
