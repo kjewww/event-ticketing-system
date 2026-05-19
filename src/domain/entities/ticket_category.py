236c5cca19a81a615c1ab2adc0ae1384a0e2b191
Tanq-1 <tanquiry@gmail.com>
2026-05-19 18:35:10 +0700

    fix: used domain exceptions

--- src/domain/entities/ticket_category.py
+++ src/domain/entities/ticket_category.py
@@ -1,6 +1,11 @@
+from uuid import uuid4
+
+from src.domain.exceptions.domain_exception import (
+    InvalidTicketCategoryNameError,
+    InvalidTicketCategoryQuotaError,
+)
 from src.domain.value_objects.date_range import DateRange
 from src.domain.value_objects.money import Money
-from uuid import uuid4
 
 
 class TicketCategory:
@@ -9,18 +14,24 @@ class TicketCategory:
         name: str,
         price: Money,
         quota: int,
-        sales_date_range: DateRange
-        ):
-        
+        sales_date_range: DateRange,
+    ):
+        if not name or not name.strip():
+            raise InvalidTicketCategoryNameError(
+                "Ticket category name is required."
+            )
+
         if quota <= 0:
-            raise ValueError("Quota must be greater than zero!")
-                
+            raise InvalidTicketCategoryQuotaError(
+                "Quota must be greater than zero."
+            )
+
         self.id = uuid4()
         self.name = name
         self.price = price
         self.quota = quota
         self.sales_date_range = sales_date_range
         self.is_active: bool = True
-        
-    def disable(self):
+
+    def disable(self) -> None:
         self.is_active = False
