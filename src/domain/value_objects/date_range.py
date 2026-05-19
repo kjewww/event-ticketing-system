fd7aa252e6e0de9eb7b7bbaff55239f6af36b776
Tanq-1 <tanquiry@gmail.com>
2026-05-19 20:33:25 +0700

    fix: fixed logic of date_range.py
    Changed logic of the validation to better suit the user stories and also allowed the contains() method to accept datetime

--- src/domain/value_objects/date_range.py
+++ src/domain/value_objects/date_range.py
@@ -2,12 +2,14 @@ from datetime import datetime, date
 
 class DateRange:
     def __init__(self, start_date: date, end_date: date):
-        
-        if start_date >= end_date:
-            raise ValueError("Start date must be before end date.")
-        
+        if end_date < start_date:
+            raise ValueError("End date cannot be earlier than start date.")
+
         self.start_date = start_date
         self.end_date = end_date
 
-    def contains(self, target_date: datetime) -> bool:
+    def contains(self, target_date: date | datetime) -> bool:
+        if isinstance(target_date, datetime):
+            target_date = target_date.date()
+
         return self.start_date <= target_date <= self.end_date
