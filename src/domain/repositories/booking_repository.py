d0814bed8956ca7297d34b35d03432502974790a
Tanq-1 <tanquiry@gmail.com>
2026-05-19 19:17:58 +0700

    feat: added booking_repository.py

---
+++ src/domain/repositories/booking_repository.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from src.domain.aggregates.booking import Booking
+
+
+class BookingRepository(ABC):
+    @abstractmethod
+    def get_by_id(self, booking_id: UUID) -> Booking | None:
+        pass
+
+    @abstractmethod
+    def save(self, booking: Booking) -> None:
+        pass
+
+    @abstractmethod
+    def has_active_booking_for_event(
+        self,
+        customer_id: str,
+        event_id: UUID,
+    ) -> bool:
+        pass
+
+    @abstractmethod
+    def get_paid_bookings_by_event_id(
+        self,
+        event_id: UUID,
+    ) -> list[Booking]:
+        pass
