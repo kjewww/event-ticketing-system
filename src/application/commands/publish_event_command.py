617bb6680fa4cb735872c1a1e1a1db0848bc5ec9
Tanq-1 <tanquiry@gmail.com>
2026-05-20 12:29:54 +0700

    feat: added publish_event_command.py

---
+++ src/application/commands/publish_event_command.py
@@ -0,0 +1,11 @@
+from uuid import UUID
+
+
+class PublishEventCommand:
+    def __init__(
+        self,
+        event_id: UUID,
+        organizer_id: str,
+    ):
+        self.event_id = event_id
+        self.organizer_id = organizer_id
