0de8fb581132800d5b8e9043346ee52c689118b5
Tanq-1 <tanquiry@gmail.com>
2026-05-20 12:44:35 +0700

    feat: added TicketCategoryDTO for details

--- src/application/dto/ticket_category_dto.py
+++ src/application/dto/ticket_category_dto.py
@@ -34,3 +34,29 @@ class DisableTicketCategoryResponseDTO:
         self.ticket_category_id = ticket_category_id
         self.name = name
         self.is_active = is_active
+
+
+class TicketCategoryDTO:
+    def __init__(
+        self,
+        ticket_category_id,
+        name,
+        price_amount,
+        currency,
+        quota,
+        remaining_quota,
+        sales_start_date,
+        sales_end_date,
+        is_active,
+        purchase_status,
+    ):
+        self.ticket_category_id = ticket_category_id
+        self.name = name
+        self.price_amount = price_amount
+        self.currency = currency
+        self.quota = quota
+        self.remaining_quota = remaining_quota
+        self.sales_start_date = sales_start_date
+        self.sales_end_date = sales_end_date
+        self.is_active = is_active
+        self.purchase_status = purchase_status
